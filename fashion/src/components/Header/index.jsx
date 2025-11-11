da93a7ccb4fd5b40aa665b82483ebf5ee3f4904b
Amit Makwana <makwanaamit985@gmail.com>
2025-11-11 22:22:00 +0530

    work

--- fashion/src/components/Header/index.jsx
+++ fashion/src/components/Header/index.jsx
@@ -1,11 +1,25 @@
+import { Link } from 'react-router-dom';
+import logo from './assets/images/logo.png';
+
+
 const Header = () => {
     return(
         <>
-           <div className="header">
-                <div className="top-strip">
+           <div className="headerwrapper">
+                <div className="top-strip bg-purple">
                 <div className="container">
-                <p className="mb-0 mt-0 text-center">Enjoy our great International Shipping Rates Details</p>
+                <p className="mb-0 mt-0 text-center">Enjoy our great International Shipping Rates </p>
                 </div>
+                </div>
+                <div className="header">
+                    <div className="container">
+                        <div className="row">
+                            <div className="logowrapper col-sm-3">
+                                 <link to={"/"}><img src={logo} alt="logo" />
+                                 </link>
+                            </div>
+                        </div>
+                    </div>
                 </div>
            </div>
         </>
