bb5794bc445df2f77bb8397adee09c6ba6195403
Amit Makwana <makwanaamit985@gmail.com>
2025-11-12 20:10:37 +0530

    work

--- fashion/src/components/Navbar/Navbar.jsx
+++ fashion/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import logo from '../assets/images/logo.png'
 import cart from '../assets/images/cart.jpg'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+
+  const [menu,setmenu] = useState("Home")
   return (
     <div className='navbar'>
      <div className='nav-logo'>
@@ -11,13 +14,14 @@ const Navbar = () => {
         <p>Men Fashion Hub</p>
      </div>
     <ul className='nav-menu'>
-        <li>SHOP</li>
-        <li>Shirt</li>
-        <li>Pent</li>
+        <li onClick={()=>{setmenu("Home")}}><Link to='/'>Home</Link>{menu==="Home"?<hr/>:<></>}</li>
+        <li onClick={()=>{setmenu("Shirts")}}><Link to='/Shirt'>Shirt</Link>{menu==="Shirt"?<hr/>:<></>}</li>
+        <li onClick={()=>{setmenu("Pents")}}><Link to='/pent'>pent</Link>{menu==="Pent"?<hr/>:<></>}</li>
     </ul>
     <div className='nav-login-cart'>
-        <button>Login</button>
-        <img src={cart} alt="" />
+        <Link to='/logit'><button>Login</button></Link>
+        <Link to='/cart'><img src={cart} alt="" /></Link>
+        <div className="nav-cart-count">0</div>
     </div>
     </div>
 
---
+++ fashion/src/pages/shopcategories.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const shopcategories = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default shopcategories
