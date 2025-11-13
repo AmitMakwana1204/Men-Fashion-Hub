b838ed7832614dd87f73a69b056009ee8c12d5de
Amit Makwana <makwanaamit985@gmail.com>
2025-11-13 13:56:37 +0530

    .

--- fashion/src/App.js
+++ fashion/src/App.js
@@ -2,27 +2,12 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
 
-import Home from './pages/Home';
-import ShopCategories from './pages/shopcategories';
-import Product from './pages/product';
-import Cart from './pages/cart';
-import Login from './pages/login';
+
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
      <Navbar />
-     <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/Shirts" element={<ShopCategories category="Shirt" />} />
-      <Route path="/Pents" element={<ShopCategories category="Pent" />} />
-      <Route path="/product" element={<Product />} />
-
-      <Route path="/cart" element={<Cart />} />
-      <Route path="/login" element={<Login />} />
-     </Routes>
-     </BrowserRouter>
     </div>
   );
 }
